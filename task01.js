1b21d111d621c81dd27072f035b86f3c399cc16d
Lougle <louglescom@gmail.com>
2021-12-30 10:21:08 +0200

    ready to work

--- task01.js
+++ task01.js
@@ -1,7 +1,3 @@
 console.group("Task 1");
-let label = "Генератор защитного поля";
-let price = 1000;
-console.log(`Выбран ${label}, цена за штуку ${price} кредитов`);
-price = 2000;
-console.log(`Выбран ${label}, цена за штуку ${price} кредитов`);
+
 console.groupEnd();
--- task04.js
+++ task04.js
@@ -1,18 +1,3 @@
 console.group("Task 4");
-let credits = 23580;
-const pricePerDroid = 3000;
-const title = "How many droids do you want to buy?";
-let message = prompt(title);
-let totalPrice = 0;
-if (message === null) {
-  message = "Отменено пользователем!";
-} else if (message * pricePerDroid > credits) {
-  message = "Недостаточно средств на счету!";
-} else if (message * pricePerDroid < credits && message.length > 0) {
-  credits = credits - message * pricePerDroid;
-  message = `Вы купили ${message} дроидов, на счету осталось ${credits} кредитов.`;
-} else {
-  message = "incorrect input";
-}
-console.log(message);
+
 console.groupEnd();
