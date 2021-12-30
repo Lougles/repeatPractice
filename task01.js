3341acb478e1f02225b9d1cc433f541e32a2dd7d
Lougle <louglescom@gmail.com>
2021-12-30 12:40:12 +0200

    all tasks are done

--- task01.js
+++ task01.js
@@ -1,3 +1,10 @@
 console.group("Task 1");
-
+const logItems = function (array) {
+  for (let i = 0; i < array.length; i += 1) {
+    console.log(`${i + 1} - ${array[i]}`);
+  }
+};
+logItems(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]);
+console.log("=====================================");
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
 console.groupEnd();
--- task04.js
+++ task04.js
@@ -1,3 +1,24 @@
 console.group("Task 4");
+const formatString = function (string) {
+  const newString = string.split("");
+  if (newString.length > 40) {
+    return `${newString.slice(0, 39).join("")}...`;
+  }
+  return newString.join("");
+};
+console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// вернется оригинальная строка
 
+console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// вернется форматированная строка
+
+console.log(formatString("Curabitur ligula sapien."));
+// вернется оригинальная строка
+
+console.log(
+  formatString(
+    "Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."
+  )
+);
+// вернется форматированная строка
 console.groupEnd();
