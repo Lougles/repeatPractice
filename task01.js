193fc2cc4de419a7b89096ce7fd909c0a5798bf0
Вова Челидзе <vovacelidze@MacBook-Air-Vova.local>
2022-01-03 22:45:59 +0200

    need to work on 2-nd task

--- task01.js
+++ task01.js
@@ -1,7 +1,28 @@
 console.group("Task 1");
-let label = "Генератор защитного поля";
-let price = 1000;
-console.log(`Выбран ${label}, цена за штуку ${price} кредитов`);
-price = 2000;
-console.log(`Выбран ${label}, цена за штуку ${price} кредитов`);
+const account = {
+    owner: 'Mango',
+    balance: 24000,
+    discount: 0.1,
+    orders: ['order-1', 'order-2', 'order-3'],
+    changeDiscount(value) {
+        this.discount = value;
+    },
+    showOrders() {
+      return this.orders;
+    },
+    addOrder(cost, order) {
+      this.balance -= cost;
+      this.orders.push(order);
+    },
+  };
+  
+  account.changeDiscount(0.15);
+  console.log(account.discount); // 0.15
+  
+  console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+  
+  account.addOrder(5000, 'order-4');
+  console.log(account.balance); // 19000
+  console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
+  
 console.groupEnd();
--- task02.js
+++ task02.js
@@ -1,12 +1,27 @@
 console.group("Task 2");
-function sklad(order, total) {
-  if (order < total) {
-    return `Вы заказали ${order} едениц товара. Заказ оформлен, с вами свяжется менеджер`;
-  } else if (order > total) {
-    return `Вы заказали ${order} единиц товара, а у нас на складе есть только ${total} единиц. Выберете другую сумму!`;
-  }
-}
-console.log(sklad(20, 100));
-console.log(sklad(80, 100));
-console.log(sklad(130, 100));
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+const invokeInventoryAction = function(itemName, action) {
+  console.log(action);
+  console.log(typeof(action));
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
+invokeInventoryAction.bind('Medkit', inventory.add);
+// Invoking action on Medkit
+// Adding Medkit to inventory
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+invokeInventoryAction.bind('Gas mask', inventory.remove);
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+console.log(inventory.items); // ['Knife', 'Medkit']
 console.groupEnd();
