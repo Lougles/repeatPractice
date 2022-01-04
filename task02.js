b611daed41270f104fdf076dffd2ab091382c8fe
Вова Челидзе <vovacelidze@MacBook-Air-Vova.local>
2022-01-04 11:59:09 +0200

    task02 is done

--- task02.js
+++ task02.js
@@ -11,16 +11,14 @@ const inventory = {
   },
 };
 const invokeInventoryAction = function(itemName, action) {
-  console.log(action);
-  console.log(typeof(action));
   console.log(`Invoking action on ${itemName}`);
   action(itemName);
 };
-invokeInventoryAction.bind('Medkit', inventory.add);
+invokeInventoryAction('Medkit',inventory.add.bind(inventory));
 // Invoking action on Medkit
 // Adding Medkit to inventory
 console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
-invokeInventoryAction.bind('Gas mask', inventory.remove);
+invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
 // Invoking action on Gas mask
 // Removing Gas mask from inventory
 console.log(inventory.items); // ['Knife', 'Medkit']
