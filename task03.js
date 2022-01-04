193b75fa7754da015bf03899f67e8be01f2a462b
Вова Челидзе <vovacelidze@MacBook-Air-Vova.local>
2022-01-04 22:43:32 +0200

    task 3 is done

--- task03.js
+++ task03.js
@@ -1,3 +1,33 @@
 console.group("Task 3");
-
+const Storage = function (arg = []){
+    this.arg = arg;
+}
+Storage.prototype.getItems = function () {
+    return this.arg;
+}
+Storage.prototype.addItem = function(item) {
+     return this.arg.push(item);
+}
+Storage.prototype.removeItem = function(item) {
+    const index = this.arg.indexOf(item);
+    if (index > -1 ){
+        return this.arg.splice(index, 1);
+    }
+}
+const storage = new Storage([
+    'Нанитоиды',
+    'Пролонгер',
+    'Железные жупи',
+    'Антигравитатор',
+  ]);
+  
+  const items = storage.getItems();
+  console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+  
+  storage.addItem('Дроид');
+  console.table(storage.getItems()); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+  
+  storage.removeItem('Пролонгер');
+  console.table(storage.getItems()); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
 console.groupEnd();
+
