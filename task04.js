269910bf032e618958e583f2da75a7ff5be31cd1
vovacelidze <louglescom@gmail.com>
2022-01-13 12:04:15 +0200

    task 02 has been updated

--- task04.js
+++ task04.js
@@ -1,3 +1,16 @@
 console.group("Task 4");
+// const StringBuilder = function(value){
+//     this._value = value;
+    
+// }
+// const builder = new StringBuilder('.');
 
+// builder.append('^');
+// console.log(builder.value); // '.^'
+
+// builder.prepend('^');
+// console.log(builder.value); // '^.^'
+
+// builder.pad('=');
+// console.log(builder.value); // '=^.^='
 console.groupEnd();
