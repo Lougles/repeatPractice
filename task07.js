b8ebd17213641ef989062a7796d2839a47707d57
vovacelidze <louglescom@gmail.com>
2022-01-14 20:06:31 +0200

    task 08 completed

--- task07.js
+++ task07.js
@@ -1,3 +1,11 @@
+import users from './users.js';
 console.group("Task 7");
+
+const calculateTotalBalance = arr => {
+    return arr.reduce((temp, user) => temp += user.balance ,0);
+  };
+  
+  console.log(calculateTotalBalance(users)); // 20916
+  
 
 console.groupEnd();
--- task08.js
+++ task08.js
@@ -1,3 +1,11 @@
+import users from './users.js';
 console.group("Task 8");
+const getUsersWithFriend = (arr, friendName) => {
+    const temp = arr.filter(user => user.friends.includes(friendName));
+    return temp.map(user => user.name);
+  };
+  
+  console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
+  console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
 console.groupEnd();
