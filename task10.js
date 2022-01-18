2cf10f6be4ea62bd52e1ac5cd7f17721806f4681
vovacelidze <louglescom@gmail.com>
2022-01-18 19:31:26 +0200

    10 task need finish

--- task10.js
+++ task10.js
@@ -1,6 +1,17 @@
+import users from './users.js';
 console.group("Task 10");
 
-
+const getSortedUniqueSkills = arr => {
+    const newArr = [];
+    arr.map(item => {
+        return newArr.push(item.skills);
+    })
+    console.log(newArr);
+  };
+  
+  console.log(getSortedUniqueSkills(users));
+  // [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+  
 
 
 
