c8b0139d1ff78083848870487ad09f06eb08fb6d
vovacelidze <louglescom@gmail.com>
2022-01-24 21:55:21 +0200

    start working

---
+++ js/task-01.js
@@ -0,0 +1,11 @@
+console.group('Task - 1');
+
+const ulRef = document.querySelector('#categories');
+
+console.log(ulRef);
+
+
+
+
+
+console.groupEnd();
--- task03.js
+++
@@ -1,8 +0,0 @@
-import users from './users.js';
-console.group("Task 3");
-
-const getUsersWithGender = (arr, gend) => {
-    return arr.filter(({gender}) => gender === gend).map(({name}) => name);
-}
-console.log(getUsersWithGender(users, 'male'));
-console.groupEnd();
