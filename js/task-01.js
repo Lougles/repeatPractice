d09846c332728db8084d0665d9f7699d1aca364d
vovacelidze <louglescom@gmail.com>
2022-01-25 13:51:44 +0200

    1-st task is done

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,27 @@
 console.group('Task - 1');
 
-const ulRef = document.querySelector('#categories');
+const findCategoryCount = str => {
+    const ref = document.querySelectorAll(str);
+    return `В списке ${ref.length} категорий.`
+}
+const findElemOfCategory = str => {
+    const objCat = {};
+    const ref = document.querySelectorAll(str);
+    for (const i of ref){
+        objCat[i.firstElementChild.innerHTML] =  i.children[1].children.length;
+        // console.log(`Категория: ${i.firstElementChild.innerHTML}`);
+        // console.log(`Количество елементов: ${i.children[1].children.length}`);
+    }
+    return objCat;
+}
 
-console.log(ulRef);
+console.log(findCategoryCount('.item'));
+console.log(findElemOfCategory('.item'));
 
-
-
-
-
+const qqq = document.querySelectorAll('.item');
+for (const i of qqq){
+    console.log(`Категория: ${i.firstElementChild.innerHTML}`);
+    console.log(`Количество елементов: ${i.children[1].children.length}`);
+}
+// console.log(qqq);
 console.groupEnd();
